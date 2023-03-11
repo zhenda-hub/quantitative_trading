96fec775a27f4239f6e45c94446792d7251085d2
zhenda <1955093734@qq.com>
2023-03-11 11:01:21 +0800

    create django web

---
+++ quan_trad/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for quan_trad project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quan_trad.settings")
+
+application = get_asgi_application()
