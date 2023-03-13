5903f4e15c80b68e8d1e62b05a0804bf22478564
zhenda <1955093734@qq.com>
2023-03-14 02:10:19 +0800

    change readme

--- login/urls.py
+++ login/urls.py
@@ -13,12 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'index'  # include() 需要设置app_name
+# urlpatterns = [
+#     path('login/', views.LoginView.as_view(), name='login'),
+#     path('register/', views.RegisterView.as_view(), name='register'),
+# ]
+
+
+
 urlpatterns = [
-    path('login/', views.LoginView.as_view(), name='login'),
-    path('register/', views.RegisterView.as_view(), name='register'),
+    # ...
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # path('logout/', auth_views..as_view(), name='logout'),
+    # ...
 ]
