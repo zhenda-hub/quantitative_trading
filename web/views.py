596dca75711197f1d3c410604096c0f42b44c0e2
zhenda <1955093734@qq.com>
2023-03-29 01:16:23 +0800

    添加bs4

--- web/views.py
+++ web/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render, redirect
 # Create your views here.
 def index(request):
     # return render(request, 'index.html')
-    return render(request, 'index2.html')
-    # return render(request, 'index3.html')
+    # return render(request, 'index2.html')
+    return render(request, 'index3.html')
     # return render(request, 'index4.html')
