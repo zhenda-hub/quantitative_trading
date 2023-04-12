bf9b0d622b8ae5ce9e321ccdc2d0cd745efeba9a
zhenda <1955093734@qq.com>
2023-04-12 08:38:12 +0800

    添加更新公告页面 header footer 页面

--- update_notice/urls.py
+++ update_notice/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 app_name = 'update_notice'
 urlpatterns = [
-    
+    path('', views.update_notice, name='update_notice')
 ]
--- update_notice/views.py
+++ update_notice/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def update_notice(request):
+    return render(request, 'update_notice.html')
