52dfc35e716a10e55766c21cf9b745e68af81ea0
zzdnb <1955093734@qq.com>
2023-05-24 23:32:55 +0800

    添加获取数据的eq方法

--- utils/get_finance_data.py
+++ utils/get_finance_data.py
@@ -1,5 +1,6 @@
 import yfinance as yf
 import datetime
+import pandas as pd
 
 
 def get_maotai():
@@ -9,6 +10,18 @@ def get_maotai():
 
     # 使用yfinance获取贵州茅台股票数据
     df = yf.download('600519.SS', start=start_date, end=end_date)
+
+
+def get_all_stock_eq():
+    """
+    使用 easyquotation 获取所有数据
+    """
+    import easyquotation
+    quotation = easyquotation.use('sina')  # 新浪 ['sina'] 腾讯 ['tencent', 'qq']
+    d1 = quotation.market_snapshot(prefix=True)  # prefix 参数指定返回的行情字典中的股票代码 key 是否带 sz/sh 前缀
+    df = pd.DataFrame(d1).T
+    df.to_excel('all_data_by_eq.xlsx')
+    d2 = quotation.real('162411')  # 支持直接指定前缀，如 'sh000001'
 
 
 def get_cn_stock_ids():
--- web/tests.py
+++ web/tests.py
@@ -1,5 +1,6 @@
 import pdb
 
+import pandas as pd
 import yfinance as yf
 from django.test import TestCase
 from django.utils.timezone import now
@@ -9,12 +10,9 @@ from quan_trad.local_settings import TUSHARE_TOKEN
 
 # Create your tests here.
 def test_get_data_yahoo():
-    import tushare as ts
-    pro = ts.pro_api(TUSHARE_TOKEN)
-    #
-    # # 获取全部可交易股票基础信息
-    df = pro.hk_basic()
+
     pdb.set_trace()
+
 
 
 
