8db9a275dfcf0453720d655a5458af4ca7be24f0
zhenda <msoft77564532@outlook.com>
2025-08-29 17:22:44 +0800

    update(dash): app nav

--- dash_web/app.py
+++ dash_web/app.py
@@ -1,5 +1,5 @@
 import dash
-from dash import Dash, html, dcc
+from dash import Dash, html
 import dash_bootstrap_components as dbc
 
 from loguru import logger
@@ -10,35 +10,51 @@ external_stylesheets = [dbc.themes.BOOTSTRAP]
 
 app = Dash(__name__, use_pages=True, external_stylesheets=external_stylesheets)
 
-app.layout = html.Div([
-    html.H1("Zhenda's quan"),
-    # dbc.Row(
-    #     [
-    #         dbc.Col(
-    #             [
-    #                 html.Div(
-    #                     dcc.Link(
-    #                         f"{page['name']} - {page['path']}", href=page["relative_path"]
-    #                     ),
-    #
-    #                 )
-    #                 for page in dash.page_registry.values()
-    #             ],
-    #             width=3
-    #         ),
-    #         dbc.Col(
-    #             dash.page_container,
-    #             width=9
-    #         ),
-    #     ]
-    # ),
+# 硬编码的自定义导航顺序
+CUSTOM_ORDER = [
+    'Home', 
+    'period', 
+    "Industry",   # 行业
+    'Index', 
+    'Company', 
+    'Bond', 
+    'Virtual',
+    'Allocation', 
+]
 
-    html.Div([
-        html.Div(
-            dcc.Link(f"{page['name']} - {page['path']}", href=page["relative_path"])
-            # dcc.Link(f"{page['name']}", href=page["relative_path"])
-        ) for page in dash.page_registry.values()  # TODO: set title
-    ]),
+# 简单按自定义顺序获取页面
+ordered_pages = []
+
+real_page_name = [page['name'] for page in dash.page_registry.values()]
+logger.debug(f'real_page_name: {real_page_name}')
+
+# breakpoint()
+for page_name in CUSTOM_ORDER:
+    for page in dash.page_registry.values():
+        # breakpoint()
+        if page['name'] == page_name:
+            ordered_pages.append(page)
+            break
+    else:
+        logger.error(f'nav page {page_name} not found')
+            # breakpoint()
+
+# 使用排序后的页面或默认顺序
+nav_pages = ordered_pages if ordered_pages else list(dash.page_registry.values())
+# breakpoint()
+app.layout = html.Div([
+    dbc.NavbarSimple(
+        children=[
+            dbc.NavItem(dbc.NavLink(page['name'], 
+                      href=page["relative_path"]))
+            for page in nav_pages
+        ],
+        brand="Zhenda's Quantitative Trading",
+        brand_href="/",
+        color="primary",
+        dark=True,
+        fluid=True
+    ),
     dash.page_container
 ])
 
