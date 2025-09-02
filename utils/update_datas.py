6b4d0e9fbc39ff3010b6cf03bd0150a588d7dc7c
zhenda <msoft77564532@outlook.com>
2025-09-02 16:42:10 +0800

    update(update_datas): log; add(update_datas): funcs

--- utils/update_datas.py
+++ utils/update_datas.py
@@ -30,7 +30,7 @@ def get_ak_stock_data():
         filename = f"datas/raw/stocks/ak_stock_list_{date_str}.csv"
         ensure_dir(filename)
         stock_info.to_csv(filename, index=False)
-        logger.info("A股列表数据已更新")
+        logger.info(f"A股列表数据: {filename} 已更新")
 
     except Exception as e:
         logger.error(f"获取akshare股票数据失败: {str(e)}")
@@ -47,13 +47,13 @@ def get_ak_industry_data():
         filename = f"datas/raw/n_indexes/ak_industry_list_{date_str}.csv"
         ensure_dir(filename)
         industry_list.to_csv(filename, index=False)
-        logger.info("行业分类数据已更新")
+        logger.info(f"行业分类数据: {filename} 已更新")
         
         # industry_list_dfcf = ak.stock_board_industry_name_em()  # 行业板块-名称， 推荐
         # industry_list_ths = ak.stock_board_industry_summary_ths()  # 同花顺行业一览表
         # ak.stock_fund_flow_industry()
         # industry_list_xl = ak.stock_sector_spot()
-        logger.info("行业分类数据已更新")
+        # logger.info("行业分类数据已更新")
 
     except Exception as e:
         logger.error(f"获取akshare行业数据失败: {str(e)}")
@@ -69,7 +69,7 @@ def get_ak_bond_data():
         filename = f"datas/raw/bonds/ak_bond_zh_cov_{date_str}.csv"
         ensure_dir(filename)
         conv_bond.to_csv(filename, index=False)
-        logger.info("可转债数据已更新")
+        logger.info(f"可转债数据: {filename} 已更新")
 
         # 国债收益率数据
         try:
@@ -77,7 +77,7 @@ def get_ak_bond_data():
             filename = f"datas/raw/bonds/ak_cn_us_rate_{date_str}.csv"
             ensure_dir(filename)
             bond_rate.to_csv(filename, index=False)
-            logger.info("中美债券收益率数据已更新")
+            logger.info(f"中美债券收益率数据: {filename} 已更新")
         except Exception as e:
             logger.error(f"获取中美债券收益率失败: {str(e)}")
 
@@ -95,14 +95,14 @@ def get_ak_fund_data():
         filename = f"datas/raw/funds/ak_etf_list_{date_str}.csv"
         ensure_dir(filename)
         etf_list.to_csv(filename, index=False)
-        logger.info("ETF基金列表已更新")
+        logger.info(f"ETF基金列表: {filename} 已更新")
 
         # 基金规模数据
         fund_aum = ak.fund_aum_em()
         filename = f"datas/raw/funds/ak_fund_aum_{date_str}.csv"
         ensure_dir(filename)
         fund_aum.to_csv(filename, index=False)
-        logger.info("基金规模数据已更新")
+        logger.info(f"基金规模数据: {filename} 已更新")
 
         # ak.fund_portfolio_industry_allocation_em()  # 基金行业配置
     except Exception as e:
@@ -147,41 +147,41 @@ def get_ak_cpi_data():
     filename = f"datas/raw/cpis/macro_china_cpi_yearly.csv"
     ensure_dir(filename)
     cpi_cn_data.to_csv(filename, index=False)
-    logger.info("中国CPI数据已更新")
+    logger.info(f"中国CPI数据: {filename} 已更新")
     
     # cpi 美国
     cpi_usa = ak.macro_usa_cpi_yoy()
     filename = f"datas/raw/cpis/macro_usa_cpi_yoy.csv"
     ensure_dir(filename)
     cpi_usa.to_csv(filename, index=False)
-    logger.info("美国CPI数据已更新")
+    logger.info(f"美国CPI数据: {filename} 已更新")
     # cpi 欧元区
     cpi_enro = ak.macro_euro_cpi_yoy()
     filename = f"datas/raw/cpis/macro_euro_cpi_yoy.csv"
     ensure_dir(filename)
     cpi_enro.to_csv(filename, index=False)
-    logger.info("欧元区CPI数据已更新")
+    logger.info(f"欧元区CPI数据: {filename} 已更新")
     
     # cpi 澳大利亚
     cpi_australia = ak.macro_australia_cpi_yearly()
     filename = f"datas/raw/cpis/macro_australia_cpi_yearly.csv"
     ensure_dir(filename)
     cpi_australia.to_csv(filename, index=False)
-    logger.info("澳大利亚CPI数据已更新")
+    logger.info(f"澳大利亚CPI数据: {filename} 已更新")
     
     # cpi 加拿大
     cpi_canada = ak.macro_canada_cpi_yearly()
     filename = f"datas/raw/cpis/macro_canada_cpi_yearly.csv"
     ensure_dir(filename)
     cpi_canada.to_csv(filename, index=False)
-    logger.info("加拿大CPI数据已更新")
+    logger.info(f"加拿大CPI数据: {filename} 已更新")
     
     # cpi 日本
     cpi_japan = ak.macro_japan_cpi_yearly()
     filename = f"datas/raw/cpis/macro_japan_cpi_yearly.csv"
     ensure_dir(filename)
     cpi_japan.to_csv(filename, index=False)
-    logger.info("日本CPI数据已更新")
+    logger.info(f"日本CPI数据: {filename} 已更新")
     
     
 def get_ak_gdp_data():
@@ -192,21 +192,21 @@ def get_ak_gdp_data():
     filename = f"datas/raw/gdps/ak_china_gdp.csv"
     ensure_dir(filename)
     china_gdp.to_csv(filename, index=False)
-    logger.info("中国GDP数据已更新")
+    logger.info(f"中国GDP数据: {filename} 已更新")
     
     # usa gdp
     usa_gdp = ak.macro_usa_gdp_monthly()  # usa gdp
     filename = f"datas/raw/gdps/ak_usa_gdp.csv"
     ensure_dir(filename)
     usa_gdp.to_csv(filename, index=False)
-    logger.info("美国GDP数据已更新")
+    logger.info(f"美国GDP数据: {filename} 已更新")
     
     # euro gdp
     enro_gdp = ak.macro_euro_gdp_yoy()  # euro gdp
     filename = f"datas/raw/gdps/ak_euro_gdp.csv"
     ensure_dir(filename)
     enro_gdp.to_csv(filename, index=False)
-    logger.info("欧元区GDP数据已更新")
+    logger.info(f"欧元区GDP数据: {filename} 已更新")
     
 
 def get_yf_market_data():
@@ -271,43 +271,10 @@ def get_ef_stock_data():
             df = ef.stock.get_realtime_quotes(k)
             filename = f"datas/raw/stocks/ef_{k}_{date_str}.csv"
             df.to_csv(filename, index=False)
-            logger.info(f"ef {k} 数据已更新")
-
-        # 获取上证50、沪深300、中证500成分股
-        index_codes = {
-            'sh000016': '上证50',
-            'sh000300': '沪深300',
-            'sh000905': '中证500'
-        }
-        
-        # df_dict = ef.stock.get_quote_history(code)
-        
-        # for code, name in index_codes.items():
-        #     try:
-            #     for period, df in df_dict.items():
-            #         fname = f"ef_{name}_{period}_{date_str}.csv"
-            #         filename = f"datas/raw/stocks/{fname}"
-            #         df.to_csv(filename, index=False)
-            #     logger.info(f"{name}历史数据已更新")
-            # except Exception as e:
-            #     logger.error(f"获取{name}历史数据失败: {str(e)}")
+            logger.info(f"ef {k} 数据: {filename} 已更新")
 
     except Exception as e:
-        logger.error(f"获取股票数据失败: {str(e)}")
-
-
-def get_ef_fund_data():
-    """获取基金相关数据"""
-    try:
-        date_str = datetime.now().strftime('%Y%m%d')
-        # 获取所有基金列表
-        fund_list = ef.fund.get_quote_history('159949')  # 获取华安创业板50ETF作为示例
-        filename = f"datas/raw/funds/ef_fund_list_{date_str}.csv"
-        fund_list.to_csv(filename, index=False)
-        logger.info("基金数据已更新")
-
-    except Exception as e:
-        logger.error(f"获取基金数据失败: {str(e)}")
+        logger.error(f"获取ef股票数据失败: {str(e)}")
 
 
 def get_ef_bond_data():
@@ -322,7 +289,7 @@ def get_ef_bond_data():
         bond_base_info = ef.bond.get_all_base_info()
         filename = f"datas/raw/bonds/ef_get_all_base_info.csv"
         bond_base_info.to_csv(filename, index=False)
-        logger.info("债券数据已更新")
+        logger.info(f"债券数据: {filename} 已更新")
 
     except Exception as e:
         logger.error(f"获取债券数据失败: {str(e)}")
@@ -337,7 +304,7 @@ def get_ef_futures_data():
         filename = f"datas/raw/futures/ef_futures_list_{date_str}.csv"
         ensure_dir(filename)
         futures_list.to_csv(filename, index=False)
-        logger.info("期货合约列表数据已更新")
+        logger.info(f"期货合约列表数据: {filename} 已更新")
 
     except Exception as e:
         logger.error(f"获取期货数据失败: {str(e)}")
@@ -348,8 +315,9 @@ def get_ak_jsl_bond():
         cookies = os.getenv('JISILU_COOKIES')
         df = ak.bond_cb_jsl(cookie=cookies)
         breakpoint()
-        df.to_csv(f'datas/raw/bonds/conv_{datetime.now().strftime("%Y%m%d")}.csv', index=False)    
-        logger.info("jsl债券数据已更新")
+        filename = f"datas/raw/bonds/conv_{datetime.now().strftime("%Y%m%d")}.csv"
+        df.to_csv(filename, index=False)    
+        logger.info(f"jsl债券数据: {filename} 已更新")
 
     except Exception as e:
         logger.error(f"获取jsl债券数据失败: {str(e)}")
@@ -361,8 +329,9 @@ def get_eq_stock_data():
     obj = eq.use("sina")
     res = obj.market_snapshot(prefix=True)
     df = pd.DataFrame(res).T.reset_index().rename(columns={"index": "code"})
-    df.to_csv(f'datas/raw/stocks/eq_stocks_{date_str}.csv', index=False)
-    logger.info("eq股票数据已更新")
+    filename = f"datas/raw/stocks/eq_stocks_{date_str}.csv"
+    df.to_csv(filename, index=False)
+    logger.info(f"eq股票数据: {filename} 已更新")
 
 
 def ana_bonds(bond_id2names: dict):
@@ -376,17 +345,34 @@ def ana_bonds(bond_id2names: dict):
         
 
 def ana_ef_fund(codes: list):
-    for code in codes:
-        ef.fund.get_base_info(code)        # 基金基本信息
-        ef.fund.get_pdf_reports(code)      # 基金公告
-        ef.fund.get_types_percentage(code)  # 基金类型占比
-        ef.stock.get_members(code)         # 获取指数的成分股
-        breakpoint()
+    """获取基金相关数据"""
+    try:
+        date_str = datetime.now().strftime('%Y%m%d')
+        
+        for code in codes:
+            ef.fund.get_pdf_reports(code)      # 基金公告, 非常老旧10年前的pdf
+            logger.info(f"{code}基金pdf, 已更新")
+            
+            df0 = ef.fund.get_base_info(code)        # 基金基本信息
+            filename = f"datas/raw/funds/ef_get_base_info_{date_str}.csv"
+            df0.to_csv(filename, index=False)
+            logger.info(f"{code} 基金基本信息: {filename} 已更新")
+            
+            df = ef.fund.get_types_percentage(code)  # 基金类型占比
+            filename = f"datas/raw/funds/ef_get_types_percentage_{date_str}.csv"
+            df.to_csv(filename, index=False)
+            logger.info(f"{code} 基金类型占比: {filename} 已更新")
+            # df2 = ef.stock.get_members(code)         # 获取指数的成分股, FIXME: notwork
+            
+
+    except Exception as e:
+        logger.error(f"获取基金数据失败: {str(e)}")
 
     
 if __name__ == "__main__":
     from utils.set_log import set_log
     set_log('update_datas.log')
+    
     
     # 更新 akshare 数据
     # get_ak_stock_data()
@@ -397,8 +383,8 @@ if __name__ == "__main__":
     
     # 更新 efinance 数据
     get_ef_stock_data()
-    # get_ef_fund_data()
     get_ef_bond_data()
+    # get_ef_fund_data()
     # get_ef_futures_data()
     
     # 更新 yfinance 数据
