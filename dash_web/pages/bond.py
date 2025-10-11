90922a552fbe1e9ab37d5f1a8a078a0743cedaa3
zhenda <msoft77564532@outlook.com>
2025-10-11 15:26:17 +0800

    add: goverment_bond

--- dash_web/pages/bond.py
+++ dash_web/pages/bond.py
@@ -98,18 +98,79 @@ def get_unlisted_bonds(df: pd.DataFrame) -> list:
     return df[df['上市日期'].isna()]['债券代码'].to_list()
 
 
+def create_yield_curve_chart(df_rate: pd.DataFrame):
+    """
+    创建国债收益率曲线图
+    """
+    # 创建收益率曲线图
+    fig = go.Figure()
+    
+    # 添加不同期限的收益率曲线
+    fig.add_trace(go.Scatter(
+        x=df_rate['日期'],
+        y=df_rate['美国国债收益率10年'],
+        mode='lines',
+        name='美国国债收益率10年',
+        line=dict(width=2)
+    ))
+    
+    fig.add_trace(go.Scatter(
+        x=df_rate['日期'],
+        y=df_rate['中国国债收益率10年'],
+        mode='lines',
+        name='中国国债收益率10年',
+        line=dict(width=3, color='red')  # 10年期用红色突出显示
+    ))
+    
+    # 更新图表布局
+    fig.update_layout(
+        title='国债10年收益率曲线',
+        xaxis_title='日期',
+        yaxis_title='收益率 (%)',
+        hovermode='x unified',
+        height=1000,
+        showlegend=True
+    )
+    fig.update_xaxes(
+        rangeslider_visible=True,  # 添加滑动块
+        minor=dict(ticks="inside", showgrid=True),  # 辅助刻度
+        # 范围选择器按钮
+        rangeselector=dict(
+            buttons=list([
+                dict(count=1, label="1month", step="month", stepmode="backward"),
+                dict(count=6, label="6month", step="month", stepmode="backward"),
+                dict(count=1, label="1year", step="year", stepmode="backward"),
+                dict(count=3, label="3year", step="year", stepmode="backward"),
+                dict(count=5, label="5year", step="year", stepmode="backward"),
+                dict(count=10, label="10year", step="year", stepmode="backward"),
+                dict(count=20, label="20year", step="year", stepmode="backward"),
+                dict(step="all")
+            ])
+        )
+    )
+    
+    return fig
+
+
 dash.register_page(__name__)
 
 # TODO: auto set file
 path = Path('datas/raw/bonds/conv_20250903.csv')
 path_old = Path('datas/raw/bonds/conv_20250825.csv')
 path_base_info = Path('datas/raw/bonds/ef_get_all_base_info.csv')
+path_rate = Path('datas/raw/bonds/ak_cn_us_rate_20250925.csv')
 
 
 df = pd.read_csv(str(path))
 df_old = pd.read_csv(str(path_old))
 df_base_info = pd.read_csv(str(path_base_info))
 unlisted_bonds = get_unlisted_bonds(df_base_info)
+
+# 加载国债收益率数据
+df_rate = pd.read_csv(str(path_rate))
+df_rate['日期'] = pd.to_datetime(df_rate['日期'])
+# 按日期排序，确保时间序列正确
+df_rate = df_rate.sort_values('日期')
 
 
 msg1, msg2, avg_low, res = judge_bonds(df)
@@ -126,14 +187,19 @@ df_old = df_old.sort_values(by=['转股价值'], ascending=[False])
 
 # breakpoint()
 
+# 创建国债收益率曲线图
+yield_curve_fig = create_yield_curve_chart(df_rate)
+
 
 layout = dbc.Container(
     [
+        dcc.Graph(figure=yield_curve_fig,),
+        html.Hr(),
         dbc.Row(
             [
                 dbc.Col(html.Div(""), width=1),
                 dbc.Col(html.Div(
-                    children=[
+                    children=[      
                         html.H4("国内可转债， 不买银行的"),
                         html.H5(f"{msg1}"),
                         html.H5(f"{msg2}"),
